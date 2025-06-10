2edc9c229f5fe61f9768e9d08b3a60e3cf38eaad
styfishnoob <main.sakana@gmail.com>
2025-06-10 14:05:35 +0900

    feat: shadcn/ui の sidebar を追加

---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarRail,
+} from "@/components/ui/sidebar";
+
+const data = {
+    navMain: [
+        {
+            title: "使い方",
+            url: "#",
+            items: [
+                {
+                    title: "Installation",
+                    url: "#",
+                },
+                {
+                    title: "Project Structure",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "機能",
+            url: "#",
+            items: [
+                {
+                    title: "個体値などから検索",
+                    url: "#",
+                },
+                {
+                    title: "Data Fetching",
+                    url: "#",
+                    isActive: true,
+                },
+            ],
+        },
+    ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar {...props}>
+            <SidebarHeader></SidebarHeader>
+            <SidebarContent>
+                {/* We create a SidebarGroup for each parent. */}
+                {data.navMain.map((item) => (
+                    <SidebarGroup key={item.title}>
+                        <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
+                        <SidebarGroupContent>
+                            <SidebarMenu>
+                                {item.items.map((item) => (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild isActive={item.isActive}>
+                                            <a href={item.url}>{item.title}</a>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                ))}
+                            </SidebarMenu>
+                        </SidebarGroupContent>
+                    </SidebarGroup>
+                ))}
+            </SidebarContent>
+            <SidebarRail />
+        </Sidebar>
+    );
+}
