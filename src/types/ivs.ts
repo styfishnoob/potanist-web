98e6a0594d0a6994cf9f81719edd522a71344d5b
styfishnoob <main.sakana@gmail.com>
2025-06-24 09:19:36 +0900

    feat: IV に関する型を定義

---
+++ src/types/ivs.ts
@@ -0,0 +1,20 @@
+export type IV = number;
+export type IVRange = { start: IV; end: IV };
+
+export type IVs = {
+    hp: IV;
+    attack: IV;
+    defense: IV;
+    speed: IV;
+    sp_attack: IV;
+    sp_defense: IV;
+};
+
+export type IVRanges = {
+    hp: IVRange;
+    attack: IVRange;
+    defense: IVRange;
+    speed: IVRange;
+    sp_attack: IVRange;
+    sp_defense: IVRange;
+};
