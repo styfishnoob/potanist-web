3c3a99c8f9584a5c24708f2b9b0bc684ac375da5
styfishnoob <main.sakana@gmail.com>
2025-07-04 11:15:52 +0900

    chore: vite-plugin-wasm を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import wasm from "vite-plugin-wasm";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss(), wasm()],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
