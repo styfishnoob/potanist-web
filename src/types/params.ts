09a47bc5ce51d0cfce1b1c53828fae658382d2da
styfishnoob <main.sakana@gmail.com>
2025-07-04 13:15:16 +0900

    feat: createSearchParams を実装

--- src/types/params.ts
+++ src/types/params.ts
@@ -1,4 +1,4 @@
-import type { IVRanges, IVs } from "./ivs";
+import type { IVRanges, IVRangesWithIgnore, IVs } from "./ivs";
 
 export type SearchParams = {
     iv_ranges: IVRanges;
@@ -23,3 +23,42 @@ export type ReturnParams = {
     hour: number;
     frame_sum: number;
 };
+
+export function createSearchParams(
+    IVRanges: IVRangesWithIgnore | null,
+    nature: string | null,
+    ability: string | null,
+    shiny: boolean | null,
+    tid: number | null,
+    sid: number | null,
+    maxAdvances: number | null,
+    maxFrameSum: number | null
+): SearchParams {
+    return {
+        iv_ranges: IVRanges
+            ? (Object.fromEntries(
+                  (Object.keys(IVRanges) as Array<keyof IVRangesWithIgnore>).map((key) => {
+                      if (IVRanges[key].ignore) {
+                          return [key, { start: 0, end: 31 }];
+                      } else {
+                          return [key, { start: IVRanges[key].start ?? 0, end: IVRanges[key].end ?? 0 }];
+                      }
+                  })
+              ) as SearchParams["iv_ranges"])
+            : {
+                  hp: { start: 31, end: 31 },
+                  attack: { start: 31, end: 31 },
+                  defense: { start: 31, end: 31 },
+                  speed: { start: 31, end: 31 },
+                  sp_attack: { start: 31, end: 31 },
+                  sp_defense: { start: 31, end: 31 },
+              },
+        nature: nature ? Number(nature) : -1,
+        ability: ability ? Number(ability) : -1,
+        shiny: shiny ?? false,
+        tid: tid ?? 0,
+        sid: sid ?? 0,
+        max_advances: maxAdvances ?? 0,
+        max_frame_sum: maxFrameSum ?? 0,
+    };
+}
