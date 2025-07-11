1c89edb2e95059dfc712c9de60b225f82740fc51
styfishnoob <main.sakana@gmail.com>
2025-07-11 14:55:53 +0900

    feat: ThemeProvider を追加

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,7 @@ import { SidebarProvider, useSidebar } from "./components/ui/sidebar";
 import { AppSidebar } from "./components/app-sidebar";
 import { Outlet, useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { ThemeProvider } from "./components/theme-provider";
 
 function SidebarController({ children }: { children: React.ReactNode }) {
     const location = useLocation();
@@ -26,16 +27,16 @@ function App() {
             window.scrollBy(0, -sidebarHeader.clientHeight);
         }
     }, [pathname]);
-    
+
     return (
-        <>
+        <ThemeProvider>
             <SidebarProvider>
                 <SidebarController>
                     <AppSidebar />
                     <Outlet />
                 </SidebarController>
             </SidebarProvider>
-        </>
+        </ThemeProvider>
     );
 }
 
