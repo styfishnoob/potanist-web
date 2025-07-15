a97c4bfd9835f8765cf60b36a4dd4e03a1e1feba
styfishnoob <main.sakana@gmail.com>
2025-07-15 18:09:06 +0900

    feat: Route を追加

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,9 @@ import { SearchStaticEncounter } from "./pages/search/static-encounter";
 import { SearchMysteryGift } from "./pages/search/mystery-gift";
 import { SearchEggPID } from "./pages/search/egg-pid";
 import { SearchEggIV } from "./pages/search/egg-iv";
+import { CheckResponseSequence } from "./pages/check/response-sequence.tsx";
+import { CheckRoamersLocation } from "./pages/check/roamers-location.tsx";
+import { CheckCoinFlip } from "./pages/check/coin-flip.tsx";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
@@ -16,9 +19,12 @@ createRoot(document.getElementById("root")!).render(
             <Routes>
                 <Route path="/" element={<App />}>
                     <Route path="/search/static-encounter" element={<SearchStaticEncounter />} />
-                    <Route path="/search/mystery-gift" element={<SearchMysteryGift />} />{" "}
-                    <Route path="/search/egg-pid" element={<SearchEggPID />} />{" "}
+                    <Route path="/search/mystery-gift" element={<SearchMysteryGift />} />
+                    <Route path="/search/egg-pid" element={<SearchEggPID />} />
                     <Route path="/search/egg-iv" element={<SearchEggIV />} />
+                    <Route path="/check/response-sequence" element={<CheckResponseSequence />} />
+                    <Route path="/check/roamers-location" element={<CheckRoamersLocation />} />
+                    <Route path="/check/coin-flip" element={<CheckCoinFlip />} />
                 </Route>
             </Routes>
         </BrowserRouter>
