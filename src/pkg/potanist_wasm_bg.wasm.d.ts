238bbc4dfefe76781da0759303750834a2ddc8f8
styfishnoob <main.sakana@gmail.com>
2025-07-16 16:04:29 +0900

    feat: wasm の pkg を追加

---
+++ src/pkg/potanist_wasm_bg.wasm.d.ts
@@ -0,0 +1,16 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const start: () => void;
+export const search_seeds_static_encounter: (a: any) => any;
+export const search_seeds_mystery_gift: (a: any) => any;
+export const search_seeds_egg_pid: (a: any, b: number) => any;
+export const search_seeds_egg_iv: (a: any, b: any, c: any) => any;
+export const create_call_response_sequence_map: (a: number, b: number, c: number) => any;
+export const create_roamers_location_map: (a: number, b: number, c: number, d: number, e: number) => any;
+export const create_coin_flip_result_map: (a: number, b: number) => any;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_export_3: WebAssembly.Table;
+export const __wbindgen_start: () => void;
